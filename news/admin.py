b3c327807b831cac60fa4eff616cf7bf3077c185
Sahrul Romadi <sahrulromadi12@gmail.com>
2024-11-28 15:03:43 +0700

    memperbaiki tata letak

--- news/admin.py
+++ news/admin.py
@@ -1,11 +1,17 @@
-# news/admin.py
-
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at')  
+    list_display = ('title', 'created_at', 'updated_at') 
+    readonly_fields = ('image_preview',)
     search_fields = ('title', 'content') 
     list_filter = ('created_at', 'updated_at',)  
     ordering = ('-updated_at',)  
+
+    def image_preview(self, obj):
+        if obj.image: 
+            return format_html('<img src="{}" style="width: 100px; height: auto;" />', obj.image.url)
+        return "No Image"
+    image_preview.short_description = "Image Preview"
